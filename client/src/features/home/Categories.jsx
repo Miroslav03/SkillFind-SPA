693cdd8f261abc4812440707ab9a4ebaab968522
Miroslav Ivanov <znaka5@abv.bg>
2024-07-18 16:43:42 +0300

    Add Categories and Join

---
+++ client/src/features/home/Categories.jsx
@@ -0,0 +1,7 @@
+export default function Categories() {
+  return (
+    <div className="h-[30rem] bg-slate-500">
+        <h1 className="text-main-text-color text-center py-16 text-2xl font-bold ">Browse by category</h1>
+    </div>
+  )
+}
---
+++ client/src/pages/join/Join.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Join() {
+  return (
+    <div className="flex w-full flex-col lg:flex-row">
+      <div className="card bg-base-300 rounded-box grid h-48 flex-grow place-items-center">
+        content
+      </div>
+      <div className="card bg-base-300 rounded-box grid h-32 flex-grow place-items-center">
+        content
+      </div>
+    </div>
+  );
+}
