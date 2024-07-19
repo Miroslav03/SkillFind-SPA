65b30e2e391fe5e0e0082a452c5d328eebab29aa
Miroslav Ivanov <znaka5@abv.bg>
2024-07-19 14:03:29 +0300

    Fix subscribe

--- client/src/features/home/Subscribe.jsx
+++ client/src/features/home/Subscribe.jsx
@@ -2,7 +2,7 @@ import Button from "../../components/ui/Button";
 
 export default function Subscribe() {
   return (
-    <div className="h-full bg-main-background-color py-16 px-[25rem] flex items-center justify-center gap-[6rem] sm:flex-col sm:px-4 sm:gap-[2rem]">
+    <div className="h-full bg-main-background-color py-16 px-[25rem] flex items-start sm:items-center justify-center gap-[6rem] sm:flex-col sm:px-4 sm:gap-[2rem]">
       <h1 className="text-5xl text-main-text-color font-bold sm:text-4xl sm:text-center">Subscribe to recive more specific jobs.</h1>
       <div>
         <form className="flex gap-2">
