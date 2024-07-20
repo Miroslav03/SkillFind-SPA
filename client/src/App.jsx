d3217b725d931ec206fdf8b8ff46c06b3fbfc526
Miroslav Ivanov <znaka5@abv.bg>
2024-07-20 18:42:55 +0300

    Add Choose page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
 import Footer from "./layouts/Footer";
 import Header from "./layouts/Header";
+import Choose from "./pages/choose/Choose";
 import Home from "./pages/home/Home";
-import Join from "./pages/join/Join";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 export default function App() {
   return (
     <>
       <Header />
-      {/* <Home /> */}
-      {/* <Join /> */}
-      <Login />
+      {/*  <Home /> */}
+      <Choose/>
+      {/* <Login /> */}
+      {/* <Register/> */}
       <Footer />
     </>
   );
---
+++ client/src/features/choose/ChooseRegiserType.jsx
@@ -0,0 +1,20 @@
+export default function ChooseRegiserType() {
+  return (
+    <div className="flex gap-8 sm:flex-col">
+        <div className="bg-main-yellow-color w-[22rem] h-[23rem] sm:w-[18rem] sm:h-[8rem] flex flex-col justify-center items-center cursor-pointer hover:bg-main-text-color group hover:scale-105 transition duration-300 ease-in-out shadow-xl">
+          <span className="material-symbols-outlined text-main-text-color text-4xl group-hover:text-white">
+            work
+          </span>
+          <h2 className="font-bold text-3xl sm:text-2xl text-white group-hover:text-main-yellow-color">
+            I'm a Client
+          </h2>
+        </div>
+        <div className="bg-main-yellow-color w-[22rem] h-[23rem] sm:w-[18rem] sm:h-[8rem] flex flex-col justify-center items-center cursor-pointer hover:bg-main-text-color group hover:scale-105 transition duration-300 ease-in-out shadow-xl">
+          <span className="material-symbols-outlined text-main-text-color text-4xl group-hover:text-white">laptop_mac</span>
+          <h2 className="font-bold text-3xl sm:text-2xl text-white group-hover:text-main-yellow-color">
+            I'm a Freelancer
+          </h2>
+        </div>
+      </div>
+  )
+}
