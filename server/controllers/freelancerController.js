ef39e06166eb34b8ad0a86c809d098ed71d2fbed
Miroslav Ivanov <znaka5@abv.bg>
2024-08-01 17:08:40 +0300

    Add login and register for freelancers

--- server/controllers/freelancerController.js
+++ server/controllers/freelancerController.js
@@ -1,3 +1,32 @@
 const router = require("express").Router();
+const freelancerService = require("../services/freelancerService");
+
+router.post("/register", async (req, res) => {
+  try {
+    const freelancerData = req.body;
+    const freelancer = await freelancerService.register(freelancerData);
+    res.status(200).json(freelancer);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred during user registration" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const freelancer = await freelancerService.login({ email, password });
+
+    if (!freelancer) {
+      res.status(401).json({ error: "Email or password is invalid" });
+    }
+
+    res.status(200).json(freelancer);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred during user login" });
+  }
+});
 
 module.exports = router;
---
+++ server/services/freelancerService.js
@@ -0,0 +1,55 @@
+const Freelancer = require("../models/users/Freelancer");
+const bcrypt = require("bcrypt");
+const { getFreelancerToken } = require("../utils/jwt");
+const { getErrors } = require("../utils/errors");
+
+exports.register = async (freelancerData) => {
+  try {
+    const freelancer = await Freelancer.create(freelancerData);
+    const freelancerToken = await getFreelancerToken(freelancer);
+
+    return {
+      _id: freelancer._id,
+      accessToken: freelancerToken,
+      name: freelancer.name,
+      email: freelancer.email,
+      title: freelancer.title,
+    };
+  } catch (error) {
+    getErrors(error);
+  }
+};
+
+exports.login = async (freelancerData) => {
+  try {
+    const freelancer = await Freelancer.findOne({
+      email: freelancerData.email,
+    });
+
+    if (!freelancer) {
+      return null;
+    }
+
+    const isValid = await bcrypt.compare(
+      freelancerData.password,
+      freelancer.password
+    );
+
+    if (!isValid) {
+      throw new Error("Email or password doesn't match");
+    }
+
+    const freelancerToken = await getFreelancerToken(freelancer);
+
+    return {
+      _id: freelancer._id,
+      accessToken: freelancerToken,
+      name: freelancer.name,
+      email: freelancer.email,
+      title: freelancer.title,
+    };
+  } catch (error) {
+    console.error(error.message);
+    throw new Error("An error occurred during login.");
+  }
+};
